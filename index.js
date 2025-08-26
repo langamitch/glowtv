df7bc5a34e8fb866d9d892548252eb1549a0a5c3
Wandile Langa <langamitch69@gmail.com>
2025-08-26 23:24:43 +0200

    updated

--- index.js
+++ index.js
@@ -149,3 +149,14 @@
   });
 
 
+
+  const nav = document.querySelector("nav");
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 50) {
+      nav.classList.add("scrolled");
+    } else {
+      nav.classList.remove("scrolled");
+    }
+  });
+
